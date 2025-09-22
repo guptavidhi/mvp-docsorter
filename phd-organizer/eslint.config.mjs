1052696a9ae4687e50a396cd3db38c4435b5bb17
Vidhi Gupta <gupta_vidhi@outlook.com>
2025-09-22 15:42:41 +0530

    lets see if vercel agress

--- phd-organizer/eslint.config.mjs
+++ phd-organizer/eslint.config.mjs
@@ -19,6 +19,10 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      // 👇 Disable the no-explicit-any rule
+      "@typescript-eslint/no-explicit-any": "off",
+    },
   },
 ];
 
